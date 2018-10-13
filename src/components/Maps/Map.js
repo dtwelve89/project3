92a09743ac29ce926b3675afa5d1e25af2fb4c72
unknown <monmaramba@yahoo.com>
2018-10-13 09:29:34 -0700

    some map css tweaks

---
+++ src/components/Maps/Map.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Map extends Component {
+    render() {
+        return(
+            <div>Map template</div>
+        );
+    }
+};
+
+export default Map;
