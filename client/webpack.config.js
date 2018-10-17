474d027682f707816c7bba3c41bd4547e1b89dc1
SHollatz <ztallohs@gmail.com>
2018-10-17 09:39:43 -0700

    issue host

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,17 +1,22 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const port = process.env.PORT || 3000;
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.[hash].js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })],
   devServer: {
     compress: true,
     inline: true,
-    port: '8080',
-    allowedHosts: [
-        '.amazonaws.com'
-    ]
+    port: port,
+    host: 'localhost'
   },
 };
